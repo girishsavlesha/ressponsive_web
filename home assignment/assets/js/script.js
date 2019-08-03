d9ff9913a7582d7064733a2b59a2e401637c4d6a
girishsavlesha <girish.savlesha@gmail.com>
2019-08-04 00:17:18 +0530

    first upload
    first edit

---
+++ home assignment/assets/js/script.js
@@ -0,0 +1,33 @@
+
+ window.onscroll = () =>{
+   var topBtn = document.getElementById('goTopBtn');
+   if(window.pageYOffset >= 300){
+      console.log(topBtn);
+       topBtn.style.display = "block" ;  
+   }
+   else{
+      topBtn.style.display = "none" ;  
+   }
+   topBtn.addEventListener("click",function(){
+      window.scrollTo(0,0);
+   });
+ };
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    nav.classList.toggle('nav-active');
+
+    navLinks.forEach((link, index) => {
+        if (link.style.animation){
+            link.style.animation='';
+
+         }else{
+             link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+0.4}s`
+         }
+
+    burger.classList.toggle('toggle');
+         
+});
+    
+}
